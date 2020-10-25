3e67921d1c6fba4b2cd7aec2533e8d229b08574d
Brad Caron <bacaron@iu.edu>
2020-10-25 12:23:22 -0400

    Update compute-stats.sh

--- compute-stats.sh
+++ compute-stats.sh
@@ -9,7 +9,7 @@ hemi="lh rh"
 [ ! -d ./output/ ] && mkdir output && cp -R ${freesurfer}/* ./output/ && chmod -R +rw ./output
 
 # copy parcellation
-[ ! -d ./parc.nii.gz ] && cp ${parc} ./parc.nii.gz && chmod +rw ./parc.nii.gz
+[ ! -d ./parc.nii.gz ] && cp ${parc} ./output/mri/parc.nii.gz && chmod +rw ./output/mri/parc.nii.gz
 
 export SUBJECTS_DIR=./
 
@@ -17,13 +17,13 @@ export SUBJECTS_DIR=./
 [ ! -f ./ribbon.nii.gz ] && mri_convert ./output/mri/ribbon.mgz ./ribbon.nii.gz
 
 # move parc into ribbon space
-[ ! -f ./parc.nii.gz ] && mri_vol2vol --mov ./parc.nii.gz --targ ./ribbon.nii.gz --regheader --interp nearest --o ./parc.nii.gz
+[ ! -f ./parc.nii.gz ] && mri_vol2vol --mov parc.nii.gz --targ ./ribbon.nii.gz --regheader --interp nearest --o ./parc.nii.gz
 
 # create hemispheric annotation files (lut needs to be created first)
 for HEMI in ${hemi}
 do
 	# create annotation file from parc volume
-	[ ! -f ./${HEMI}.parc.annot ] && mris_sample_parc -ct ./lut.txt output ${HEMI} ./parc.nii.gz ./${HEMI}.parc.annot
+	[ ! -f ./${HEMI}.parc.annot ] && mris_sample_parc -ct ./lut.txt output ${HEMI} parc.nii.gz ./${HEMI}.parc.annot
 
 	# compute stats
 	[ ! -f ./${HEMI}.parc.stats ] && mris_anatomical_stats -a ./${HEMI}.parc.annot -f ./${HEMI}.parc.stats output ${HEMI}
