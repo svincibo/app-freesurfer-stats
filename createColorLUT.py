db12fc7972f802e0d88db30d553b372adfd61cf6
Brad Caron <bacaron@iu.edu>
2020-12-03 16:55:51 -0500

    reason: fix to point to label.json

--- createColorLUT.py
+++ createColorLUT.py
@@ -7,16 +7,13 @@ import numpy as np
 def createColorLUT(keyfile,outfile):
 	
 	# load key.txt file from parcellation datatype
-	df = pd.read_csv(keyfile,delimiter='\t',header=None)
+	df = pd.read_json(keyfile)
 
 	# extract relevant data
-	df = df[[2,3]]
+	df = df[['voxel_value','name']]
 
 	# rename the columns to freesurfer format
-	df = df.rename(columns={2: '#No.',3: 'Label Name:'})
-
-	# reformat label names to remove unnecessary spaces and equal signs
-	df['Label Name:'] = [ f.split('== ')[1].split(' ')[0] for f in df['Label Name:'] ]
+	df = df.rename(columns={'voxel_value': '#No.','name': 'Label Name:'})
 
 	# generate red, green, blue and alpha values
 	df['R'] = [np.random.random_integers(0,255,size=1).tolist()[0] for f in range(len(df['#No.']))]
@@ -34,7 +31,7 @@ def main():
 		config = json.load(config_f)
 
 	# parcellation key file
-	keyfile = config['key']
+	labelfile = config['label']
 
 	# outfile for color lut
 	outfile = 'lut.txt'
